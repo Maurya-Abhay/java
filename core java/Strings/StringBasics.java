5dd15b67beca02d3223f3ecea605aba57ed9b5d7
Abhay Prasad <167536919+Maurya-Abhay@users.noreply.github.com>
2026-02-16 17:08:26 +0530

    Strings in java

---
+++ core java/Strings/StringBasics.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringBasics {
+    public static void main(String[] args) {
+        String name = "Abhay";
+        String surname = "Kumar";
+        String fullName = name +" "+ surname;
+        System.out.println(fullName);
+    }
+}
